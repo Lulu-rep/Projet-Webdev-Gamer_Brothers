447ee5f348f86a0114a9f71a02eef5d0f48cd4ec
Achecas <103190340+Achecas@users.noreply.github.com>
2022-06-06 11:02:33 +0200

    Update voiture.js

--- JS/voiture.js
+++ JS/voiture.js
@@ -6,14 +6,14 @@ function update_voiture(){
         nom="Le Panzer IV";
         marque="Krupp";
         moteur="12 cylindre en V de 11.88L de 310ch et 784Nm de couple";
-        prix="103 462 RM pour vos envie de conquêtes";
+        prix="5 000€ pour vos envie de conquêtes";
     }
     else if(voiture=="etron"){
         img="../IMG/Etron.jpg";
         nom="La E-tron";
         marque="Audi";
         moteur="2 moteur électrique de 408ch cumulé et 664Nm de couple";
-        prix="93 700€ soit beaucoup trop pour un nom pareil"
+        prix="254€ soit beaucoup trop pour un nom pareil"
     }
     else if(voiture=="multipla"){
         img="../IMG/multipla.jpg";
@@ -27,14 +27,14 @@ function update_voiture(){
         nom="La grosse caisse de oui-oui";
         marque="Reliant";
         moteur="4 cylindres en ligne de 848cm3 de 40ch et 63Nm de couple";
-        prix="12 000 livres Sterling ou ton âme offerte à la voiture"
+        prix="5€ mais il faut garder la peluche à sa place"
     }
     else{
         img="../IMG/cybertruck.jpg";
         nom="Le cyberTruck";
         marque="Tesla";
         moteur="Tri-moteur électrique entre 250 et 300 ch avec entre 400 et 500 Nm de couple";
-        prix="5 pierres pour nettoyer les vitres";
+        prix="1000€ ";
     }
     document.getElementById("voiture_img").src=img;
     document.getElementById("nom").innerText=nom;
